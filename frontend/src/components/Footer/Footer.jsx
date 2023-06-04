e327712699d09c3830d77b62759eebd73dc5e44c
sefithedev <sefithedev@gmail.com>
2023-06-04 10:47:04 +0300

    Added Newsletter and Footer components

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -2,7 +2,27 @@ import React from "react";
 import styles from "./Footer.module.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.col}>
+          <h2>About the App</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta
+            voluptas quasi accusantium voluptate nam doloremque reprehenderit
+            laboriosam nisi consectetur voluptatum, consequuntur sint recusandae
+            inventore nobis deserunt distinctio cupiditate perspiciatis illo!
+          </p>
+        </div>
+        <div className={styles.col}>
+          <h2>Contacts</h2>
+          <span>Phone: +123 456 789</span>
+          <span>YouTube: SefiTheDev</span>
+          <span>GitHub: SefiTheDev</span>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- frontend/src/components/Newsletter/Newsletter.jsx
+++ frontend/src/components/Newsletter/Newsletter.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { FiSend } from "react-icons/fi";
 import styles from "./Newsletter.module.css";
 
 const Newsletter = () => {
-  return <div>Newsletter</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.titles}>
+          <h5>Want to get our latest offers?</h5>
+          <h2>Send us your email and we will do the rest</h2>
+        </div>
+        <div className={styles.inputContainer}>
+          <input type="email" placeholder="Type email..." />
+          <FiSend className={styles.sendIcon}></FiSend>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Newsletter;
