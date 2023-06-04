4ed76eb8b2b272f247f1e858c09844bbc1407f1a
sefithedev <sefithedev@gmail.com>
2023-06-04 10:16:27 +0300

    added popular and featured properties components

--- frontend/src/components/FeaturedProperties/FeaturedProperties.jsx
+++ frontend/src/components/FeaturedProperties/FeaturedProperties.jsx
@@ -1,8 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./FeaturedProperties.module.css";
+import { request } from "../../utils/fetchAPI";
+import { Link } from "react-router-dom";
+import { FaBed, FaSquareFull } from "react-icons/fa";
+import img from "../../assets/estate3.jpg";
+import person from "../../assets/person.jpg";
 
 const FeaturedProperties = () => {
-  return <div>FeaturedProperties</div>;
+  const [featuredProperties, setFeaturedProperties] = useState([]);
+
+  useEffect(() => {
+    const fetchProperties = async () => {
+      try {
+        const data = await request("/property/get-featured", "GET");
+        setFeaturedProperties(data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchProperties();
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.titles}>
+          <h5>Properties we think you would like</h5>
+          <h2>Oure featured properties</h2>
+        </div>
+        <div className={styles.featuredProperties}>
+          {featuredProperties?.map((property) => (
+            <div key={property._id} className={styles.featuredProperty}>
+              <Link
+                to={`/propertyDetials/${property._id}`}
+                className={styles.imgContainer}
+              >
+                <img src={img} alt="" />
+              </Link>
+
+              <div className={styles.details}>
+                <div className={styles.priceAndOwner}>
+                  <span className={styles.price}>$ {property?.price}</span>
+                  <img src={person} alt="" className={styles.owner} />
+                </div>
+                <div className={styles.moreDetails}>
+                  <span>
+                    {property?.beds} beds{" "}
+                    <FaBed className={styles.icon}></FaBed>
+                  </span>
+                  <span>
+                    {property?.sqmeters} sqaure meters{" "}
+                    <FaSquareFull className={styles.icon}></FaSquareFull>
+                  </span>
+                </div>
+                <div className={styles.desc}>{property.desc}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FeaturedProperties;
--- frontend/src/components/PopularProperties/PopularProperties.jsx
+++ frontend/src/components/PopularProperties/PopularProperties.jsx
@@ -1,8 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import styles from "./PopularProperties.module.css";
+import img1 from "../../assets/realestatebeach.jpg";
+import img2 from "../../assets/realestatemountain.jpg";
+import img3 from "../../assets/realestatecountryside.jpg";
+import { request } from "../../utils/fetchAPI";
 
 const PopularProperties = () => {
-  return <div>PopularProperties</div>;
+  const [numProperties, setNumProperties] = useState({});
+
+  useEffect(() => {
+    const fetchNumProperties = async () => {
+      try {
+        const data = await request("/property/get-count", "GET");
+        setNumProperties(data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchNumProperties();
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.titles}>
+          <h5>Several property types to choose from</h5>
+          <h2>Some of our popular properties</h2>
+        </div>
+        <div className={styles.properties}>
+          <Link
+            className={styles.property}
+            to={`/properties?type=beach&continent=1&priceRange=2`}
+          >
+            <img src={img1} alt="" />
+            <div className={styles.quantity}>
+              {numProperties?.beach} properties
+            </div>
+            <h5>Beach properites</h5>
+          </Link>
+          <Link
+            className={styles.property}
+            to={`/properties?type=beach&continent=1&priceRange=2`}
+          >
+            <img src={img2} alt="" />
+            <div className={styles.quantity}>
+              {numProperties?.mountain} properties
+            </div>
+            <h5>Mountain properites</h5>
+          </Link>
+          <Link
+            className={styles.property}
+            to={`/properties?type=beach&continent=1&priceRange=2`}
+          >
+            <img src={img3} alt="" />
+            <div className={styles.quantity}>
+              {numProperties?.village} properties
+            </div>
+            <h5>Village properites</h5>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PopularProperties;
